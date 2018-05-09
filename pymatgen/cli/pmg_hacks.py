feb282c7cac895ebbcec9a37bf95b04454a90d37
Mark Turiansky <mark.turiansky@gmail.com>
2018-05-08 21:20:27 -0700

    setting up arguments

---
+++ pymatgen/cli/pmg_hacks.py
@@ -0,0 +1,17 @@
+from __future__ import division, unicode_literals
+
+'''
+a convenience script for doing things I do all the damn time
+'''
+
+
+__author__ = "Mark Turiansky"
+__copyright__ = "Copyright 2018, Mark Turiansky"
+__version__ = "4.0"
+__maintainer__ = "Shyue Ping Ong"
+__email__ = "ongsp@ucsd.edu"
+__date__ = "May 08 2018"
+
+
+def bs(args):
+    pass
